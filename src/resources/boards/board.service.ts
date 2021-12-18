342ab2b96cf54b9eaf490be236ff91366b53cea3
Юдин Алексей Михайлович <amyudin6@mts.ru>
2021-12-18 15:40:58 +0300

    feat: add tsdoc for services

--- src/resources/boards/board.service.ts
+++ src/resources/boards/board.service.ts
@@ -3,7 +3,16 @@ import { taskService } from '../tasks/task.service';
 import { boardRepository } from './board.memory.repository';
 import { Board } from './board.model';
 
+/**
+ * @remarks extend {@link BaseService} and add specific method for Boards
+ * @typeParam Board - model of the {@link Board}
+ */
 class BoardService extends BaseService<Board> {
+  /**
+   * Async func remove board and connected tasks.
+   *
+   * @param boardId - The id of board for deletion
+   */
   async removeByIdWithConnectedTasks(boardId: string) {
     const allTasks = await taskService.getAll();
     const connectedTasks = allTasks.filter((task) => task.boardId === boardId);
--- src/resources/users/user.service.ts
+++ src/resources/users/user.service.ts
@@ -3,7 +3,16 @@ import { taskService } from '../tasks/task.service';
 import { userRepository } from './user.memory.repository';
 import { User } from './user.model';
 
+/**
+ * @remarks extend {@link BaseService} and add specific method for Users
+ * @typeParam User - model of the {@link User}
+ */
 class UserService extends BaseService<User> {
+  /**
+   * Async func delete user and cancel the assignment of all his tasks
+   *
+   * @param userId - The id of user for deletion
+   */
   async removeByIdAndUnassignConnectedTasks(userId: string) {
     const allTasks = await taskService.getAll();
     const connectedTasks = allTasks.filter((task) => task.userId === userId);
