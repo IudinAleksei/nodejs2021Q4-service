051a862a8afa05d76a92828f7259a1ab31c0a1b5
Юдин Алексей Михайлович <amyudin6@mts.ru>
2022-01-16 12:07:54 +0300

    feat: create user db migration

---
+++ src/migrations/CreateUserTable.ts
@@ -0,0 +1,37 @@
+/* eslint-disable class-methods-use-this */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUserTable1642321903527 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    console.log('up');
+    await queryRunner.createTable(
+      new Table({
+        name: 'User',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'login',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+        ],
+      }),
+      true
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    console.log('down');
+  }
+}
