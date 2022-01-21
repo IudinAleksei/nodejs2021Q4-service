ab383432a79517f0049a2f0858661dee892837c1
Юдин Алексей Михайлович <amyudin6@mts.ru>
2022-01-21 12:50:26 +0300

    feat: add entities modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { RouterModule } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
+import { ColumnModule } from './column/column.module';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    TaskModule,
+    ColumnModule,
+    BoardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
