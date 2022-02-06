091bcf58b982a7acadc4ae29bbfd964adc5cc70d
Юдин Алексей Михайлович <amyudin6@mts.ru>
2022-02-06 15:09:54 +0300

    feat: add artillery script and fix linter error

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { User } from './entities/user.entity';
 
 @Module({
   imports: [PrismaModule],
